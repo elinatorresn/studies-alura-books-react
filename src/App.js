4f0580c999f680d7165f1fb38058db8ada33696e
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-11-02 19:22:12 -0300

    add latest books releases

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Header from './components/Header';
 import Search from './components/Search';
+import LatestReleases from './components/LatestReleases';
 import styled from 'styled-components'
+
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg, #002f52 35%, #326589);
 `
 
 function App() {
   return (
     <AppContainer>
-      <Header/>
+      <Header />
       <Search />
+      <LatestReleases />
     </AppContainer>
   );
 }
