a0957f4b22b966a92d8a84c616da71677f57816f
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-11-02 19:42:01 -0300

    add title props

--- src/components/LatestReleases/index.js
+++ src/components/LatestReleases/index.js
@@ -1,21 +1,12 @@
 import { books } from './dataReleases'
 import styled from 'styled-components'
+import { Title } from '../Title'
 
 const LatestReleasesContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Title = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NewBooksContainer = styled.div`
@@ -29,7 +20,7 @@ const NewBooksContainer = styled.div`
 function LatestReleases() {
   return (
     <LatestReleasesContainer>
-      <Title>Últimos Lançamentos</Title>
+      <Title color="#EB9B00" fontSize="36px">Últimos Lançamentos</Title>
       <NewBooksContainer>
         {books.map(  book => (
           <img src={book.src} alt='Livro'/>
