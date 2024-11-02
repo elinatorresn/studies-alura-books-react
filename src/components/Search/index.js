70a0947c431919607700b952f42993aecb8403b0
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-11-01 22:07:46 -0300

    add state onBlur and search results

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,7 @@
 import Input from '../Input';
 import styled from 'styled-components'
 import { useState } from 'react';
+import { books } from './dataSearch'
 
 const SearchSection = styled.section`
   background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -22,10 +23,39 @@ const SubTitle = styled.h3`
   font-size: 16px;
   font-weight: 500;
   margin-bottom: 40px;
-`     
+`
+
+const BookContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+      width: 200px;
+    }
+    img {
+      width: 180px;
+    }
+    &:hover {
+      transform: scale(1.05);
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
+    &:hover img {
+        transform: scale(1.1);
+    }
+`
 
 function Search() {
-  const [typedText, setTypedText] = useState('')
+  const [researchedBooks, setResearchedBooks] = useState([])
 
   return(
     <SearchSection>
@@ -33,9 +63,20 @@ function Search() {
       <SubTitle>Encontre seu livro em nossa estante</SubTitle>
       <Input
         type="text" placeholder='Informe sua próxima leitura'
-        onBlur={event => setTypedText(event.target.value)}
+        onBlur={event => {
+          const typedText = event.target.value
+          const searchResult = books.filter(book => book.name.includes(typedText))
+          setResearchedBooks(searchResult)
+        }}
       />
-      <p>{typedText}</p>
+      <BookContainer>
+        { researchedBooks.map(  book => (
+          <Resultado>
+            <p>{book.name}</p>
+            <img src={book.src}/>
+          </Resultado>
+        ) ) }
+      </BookContainer>
     </SearchSection>
     
   )
